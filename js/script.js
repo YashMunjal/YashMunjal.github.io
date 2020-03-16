41bd8d550e8a164b1344c554ccb97fedfcba2b5d
YashMunjal <yashmu45@gmail.com>
2020-03-16 16:45:07 +0530

    console

--- js/script.js
+++ js/script.js
@@ -3,14 +3,6 @@
 
 
 
-    // Sticky Menu
-    $(window).scroll(function () {
-        if ($('.navigation').offset().top > 100) {
-            $('.navigation').addClass('nav-bg');
-        } else {
-            $('.navigation').removeClass('nav-bg');
-        }
-    });
 
     // Background-images
     $('[data-background]').each(function () {
